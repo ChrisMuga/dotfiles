a1de9eb67ea8ea0921b21f0e52a2fbae8b4eac0e
Chris Muga <ChrisMuga94@Gmail.com>
2025-01-10 18:25:06 +0300

    Working on cursors

--- run.sh
+++ run.sh
@@ -2,18 +2,26 @@ echo "Resetting config"
 
 echo "Alarcritty..."
 cp -r alacritty/ ~/.config/alacritty
+echo "------"
 echo "Ghostty..."
 cp -r ghostty/ ~/.config/ghostty
+echo "------"
 echo "Kitty..."
 cp -r kitty/ ~/.config/kitty
+echo "------"
 echo "Nvim..."
 cp -r nvim/ ~/.config/nvim
+echo "------"
 echo "Tmux..."
 cp -r tmux/ ~/.config/tmux
+echo "------"
 echo "Zellij..."
 cp -r zellij/ ~/.config/zellij
+echo "------"
 echo "Starship..."
 cp starship.toml ~/.config/starship.toml
+echo "------"
 echo "Wezterm..."
 cp .wezterm.lua ~/.wezterm.lua
+echo "------"
 echo "Done 👍"
