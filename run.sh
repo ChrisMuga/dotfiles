8d6c09f29318f39b8df5ae56866d16865dccef52
Chris Muga <ChrisMuga94@Gmail.com>
2025-01-10 15:00:50 +0300

    Add shell script

---
+++ run.sh
@@ -0,0 +1,18 @@
+echo "Resetting config"
+
+echo "Alarcritty..."
+cp -r alacritty/ ~/.config/alacritty
+echo "Ghostty..."
+cp -r ghostty/ ~/.config/ghostty
+echo "Kitty..."
+cp -r kitty/ ~/.config/kitty
+echo "Nvim..."
+cp -r nvim/ ~/.config/nvim
+echo "Tmux..."
+cp -r tmux/ ~/.config/tmux
+echo "Zellij..."
+cp -r zellij/ ~/.config/zellij
+echo "Starship..."
+cp starship.toml ~/.config/starship.toml
+
+echo "Done 👍"
