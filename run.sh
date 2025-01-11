524add9244a9dd52e851302f25a63703fa1c088b
Chris Muga <ChrisMuga94@Gmail.com>
2025-01-12 01:23:01 +0300

    Default to fetching nvim repo

--- run.sh
+++ run.sh
@@ -2,34 +2,34 @@ echo "Resetting config"
 
 echo "Alarcritty..."
 rm -rf ~/.config/alacritty
-cp -R alacritty/ ~/.config/
+cp -R alacritty/ ~/.config/alacritty/
 echo "------"
 echo "Ghostty..."
 rm -rf ~/.config/ghostty
-cp -R ghostty/ ~/.config/
+cp -R ghostty/ ~/.config/ghostty/
 echo "------"
 echo "Kitty..."
 rm -rf ~/.config/kitty
-cp -R kitty/ ~/.config/
+cp -R kitty/ ~/.config/kitty/
 echo "------"
 echo "Nvim..."
 rm -rf ~/.config/nvim
-cp -R nvim/ ~/.config/
+git clone git@github.com:ChrisMuga/nvim.git ~/.config/nvim
 echo "------"
 echo "Tmux..."
 rm -rf ~/.config/tmux
-cp -R tmux/ ~/.config/
+cp -R tmux/ ~/.config/tmux/
 echo "------"
 echo "Zellij..."
 rm -rf ~/.config/zellij
-cp -R zellij/ ~/.config/
+cp -R zellij/ ~/.config/zellij/
 echo "------"
 echo "Starship..."
-rm ~/.config/starship.toml
-cp starship.toml ~/.config/
+rm -f ~/.config/starship.toml
+cp starship.toml ~/.config/starship.toml
 echo "------"
 echo "Wezterm..."
-rm ~/.wezterm.lua
+rm -f ~/.wezterm.lua
 cp .wezterm.lua ~/.wezterm.lua
 echo "------"
 echo "Done 👍"
