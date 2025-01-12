1becebbe970f72a2d8af14b89cdebcbb2b2959fb
Chris Muga <ChrisMuga94@Gmail.com>
2025-01-12 10:44:04 +0300

    Refactor to call functions from Makefile

--- Makefile
+++ Makefile
@@ -1,11 +1,39 @@
 run:
 	. ./run.sh
 .SILENT:
+run-tmux:
+	rm -rf ~/.config/tmux
+	cp -R tmux/ ~/.config/tmux/
+	echo "TMUX ✅"
 run-kitty:
 	rm -rf ~/.config/kitty
 	cp -R kitty/ ~/.config/kitty/
 	echo "Kitty ✅"
+run-alacritty:
+	rm -rf ~/.config/alacritty
+	cp -R alacritty/ ~/.config/alacritty/
+	echo "Alacritty ✅"
 run-ghostty:
-	echo "Ghostty ✅"
 	rm -rf ~/.config/ghostty
 	cp -R ghostty/ ~/.config/ghostty/
+	echo "Ghostty ✅"
+run-nvim:
+	rm -rf ~/.config/nvim
+	git clone git@github.com:ChrisMuga/nvim.git ~/.config/nvim
+	echo "NVIM ✅"
+run-lazygit:
+	rm -rf ~/.config/lazygit
+	cp -R lazygit/ ~/.config/lazygit/
+	echo "LazyGit ✅"
+run-zellij:
+	rm -rf ~/.config/zellij
+	cp -R zellij/ ~/.config/zellij/
+	echo "Zellij ✅"
+run-starship:
+	rm -f ~/.config/starship.toml
+	cp starship.toml ~/.config/starship.toml
+	echo "Starship ✅"
+run-wezterm:
+	rm -f ~/.wezterm.lua
+	cp .wezterm.lua ~/.wezterm.lua
+	echo "Wezterm ✅"
--- run.sh
+++ run.sh
@@ -1,35 +1,14 @@
 echo "Resetting config"
 
-echo "Alarcritty..."
-rm -rf ~/.config/alacritty
-cp -R alacritty/ ~/.config/alacritty/
 echo "------"
-echo "Ghostty..."
-rm -rf ~/.config/ghostty
-cp -R ghostty/ ~/.config/ghostty/
-echo "------"
-echo "Kitty..."
-rm -rf ~/.config/kitty
-cp -R kitty/ ~/.config/kitty/
-echo "------"
-echo "Nvim..."
-rm -rf ~/.config/nvim
-git clone git@github.com:ChrisMuga/nvim.git ~/.config/nvim
-echo "------"
-echo "Tmux..."
-rm -rf ~/.config/tmux
-cp -R tmux/ ~/.config/tmux/
-echo "------"
-echo "Zellij..."
-rm -rf ~/.config/zellij
-cp -R zellij/ ~/.config/zellij/
-echo "------"
-echo "Starship..."
-rm -f ~/.config/starship.toml
-cp starship.toml ~/.config/starship.toml
-echo "------"
-echo "Wezterm..."
-rm -f ~/.wezterm.lua
-cp .wezterm.lua ~/.wezterm.lua
+make run-alacritty -s
+make run-ghostty -s
+make run-kitty -s
+make run-tmux -s
+make run-zellij -s
+make run-nvim -s
+make run-lazygit -s
+make run-starship -s
+make run-wezterm -s
 echo "------"
 echo "Done 👍"
