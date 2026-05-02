208090c7b9235ee07fd4ee5105fca12f2d290517
Chris Muga <ChrisMuga94@Gmail.com>
2026-05-02 03:39:29 +0300

    more specification on theme behaviour

--- theme/src/main.c
+++ theme/src/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-const char SET_GHOSTTY_THEME_LIGHT[] = "theme = Ayu Light";
-const char SET_GHOSTTY_THEME_DARK[] = "theme = Ayu";
+const char SET_GHOSTTY_THEME_LIGHT[] = "theme = Ayu Light\n";
+const char SET_GHOSTTY_THEME_DARK[] = "theme = Ayu\n";
+
+const char SET_NVIM_THEME_LIGHT[] = "theme = \"ayu-light\"\n";
+const char SET_NVIM_THEME_DARK[] = "theme = \"ayu-dark\"\n";
 
 const char CONFIG_FILE_PATH_GHOSTTY[] = ".config/ghostty/config";
-const char CONFIG_FILE_PATH_NVIM[] = "~/.config/nvim/lua/set-theme.lua";
+const char CONFIG_FILE_PATH_NVIM[] = ".config/nvim/lua/set-theme.lua";
 
 // TODO:
 //	- Find get the path of the config files, i.e:
@@ -13,32 +17,62 @@ const char CONFIG_FILE_PATH_NVIM[] = "~/.config/nvim/lua/set-theme.lua";
 //  		- nvim -> ~/.config/nvim/lua/set-theme.lua
 
 int main(int argc, char **args) {
-  printf("Your's is the world, and everything that's in it.\n");
   const char *home = getenv("HOME");
 
-  // printf("----> %d, ^ %s", argc, args[1]);
-  char theme[] = "light";
+  char ghostty_command[100];
+  char nvim_command[100];
+
+  if (argc > 1) {
+    if (strcmp(args[1], "dark") == 0) {
+      strcpy(ghostty_command, SET_GHOSTTY_THEME_DARK);
+      strcpy(nvim_command, SET_NVIM_THEME_DARK);
+    } else {
+      strcpy(ghostty_command, SET_GHOSTTY_THEME_LIGHT);
+      strcpy(nvim_command, SET_NVIM_THEME_LIGHT);
+    }
+  } else {
+    strcpy(ghostty_command, SET_GHOSTTY_THEME_LIGHT);
+    strcpy(nvim_command, SET_NVIM_THEME_LIGHT);
+  }
 
   if (home == NULL) {
     printf("Cannot access path\n");
     return 0;
   }
 
-  char path[1000];
+  char ghostty_path[1000];
+  char nvim_theme_path[1000];
 
-  sprintf(path, "%s/%s", home, CONFIG_FILE_PATH_GHOSTTY);
+  sprintf(ghostty_path, "%s/%s", home, CONFIG_FILE_PATH_GHOSTTY);
+  sprintf(nvim_theme_path, "%s/%s", home, CONFIG_FILE_PATH_NVIM);
 
-  FILE *fptr;
+  // Ghostty config
+  FILE *fptr_ghostty;
 
-  fptr = fopen(path, "a");
+  fptr_ghostty = fopen(ghostty_path, "a");
 
-  if (fptr == NULL) {
-    printf("Cannot open file\n");
+  if (fptr_ghostty == NULL) {
+    printf("Cannot access ghostty config file\n");
     return 0;
   }
 
-  printf("----> %s\n", path);
-  fprintf(fptr, SET_GHOSTTY_THEME_DARK);
+  fprintf(fptr_ghostty, "%s", ghostty_command);
+
+  fclose(fptr_ghostty);
+
+  // Nvim config
+  FILE *fptr_nvim;
+
+  fptr_nvim = fopen(nvim_theme_path, "a");
+
+  if (fptr_nvim == NULL) {
+    printf("Cannot access nvim config file\n");
+    return 0;
+  }
+
+  fprintf(fptr_nvim, "%s", nvim_command);
+
+  fclose(fptr_nvim);
 
   return 0;
 }
